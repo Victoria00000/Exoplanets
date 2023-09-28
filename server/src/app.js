b81fcfb6d40a28f8b84157242e54e94c5eeb7d9d
Victoria <vvictoriaandres@gmail.com>
2023-09-28 17:37:15 -0300

    creating the first model, Router, controller, for getAllplanets

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 // This is CommonJS + Express
 const express = require("express"); // or in +ES6: import express from 'express';
+const planetsRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
+
+app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
