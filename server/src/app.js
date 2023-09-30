28a0a60fc9ed51e39657bb50cd7906a5570ebc3c
Victoria <vvictoriaandres@gmail.com>
2023-09-30 19:29:42 -0300

    building the main model.planets.js (function isHabitablePlanet + function loadPlanetsData + module.exports  planets: habitablePlanets), adding cors to app.js and whitelisting localhost:3000

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 // This is CommonJS + Express
 const express = require("express"); // or in +ES6: import express from 'express';
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(planetsRouter);
 
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+const parse = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (data) => {
+        if (isHabitablePlanet(data)) {
+          savePlanet(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  planets: habitablePlanets,
+};
